39fc4e5a7a62b5e336ba5c9e6225b613529248cb
Miguel Pena Garcia <miguep@microsoft.com>
2019-05-28 15:28:21 -0700

    Repro for clipboard regression, both WinForms and WPF scenarios are broken

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows; // Use WPF
+//using System.Windows.Forms; // Use Winforms
+
+namespace ClipboardRegressionConsole
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Clipboard.SetData(DataFormats.UnicodeText, "abc");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
